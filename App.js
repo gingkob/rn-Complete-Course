6a58aee2dd921819981ec58746dfe0ef503054e3
gingkob <malivlada@hotmail.com>
2020-06-18 20:45:00 +0200

    just starting with layout

--- App.js
+++ App.js
@@ -1,19 +1,47 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+
+  const[text, setText] = useState("Flower")
+
+
+  const options = ["Flower", "Katarina", "Ljubav", "Love"]
+
+  const changeText = () => {
+    let index = parseInt(Math.random()*4)
+    setText(options[index])
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <TextInput style={styles.textInput} placeholder="Course Goal"/>
+        <Button title="Click me!" onPress={changeText} color="green"/>
+      </View>
+        <View style={styles.body}>
+        <Text>{text}</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding:30
   },
+  header: {
+    flexDirection:"row",
+    alignItems: "center",
+    justifyContent:"space-evenly",
+    marginVertical:10,
+  },
+  body: {
+    backgroundColor: 'gray'
+  },
+  textInput: {
+    width: "70%",
+    borderBottomColor: "green",
+    borderBottomWidth:3
+  }
 });
