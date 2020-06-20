b76f8bc1f2d77f59129ff7fc84c5396884064184
gingkob <malivlada@hotmail.com>
2020-06-20 17:15:30 +0200

    added finished list container

--- App.js
+++ App.js
@@ -7,16 +7,45 @@ export default function App() {
 
   const [text, setText] = useState("")
   const [goals, setGoal] = useState([])
+  const [finishedGoals, setFinishedGoal] = useState([])
+  const [showFinished, setShowFinished] = useState(true);
+
   const addGoal = () => {
-    setGoal(goals => [...goals, { key: Math.random().toString(), value: text }])
+    setGoal(goals => [{ key: Math.random().toString(), value: text }, ...goals])
     setText("")
   }
 
-  const removeGoal = index => {
+  /*  const finishGoal = (finishedGoal) => {
+     setFinishedGoal(finishedGoals => [...finishedGoals, finishedGoal])
+   } */
 
+  const removeGoal = index => {
     let goal = goals.filter(item => item.key != index)
     setGoal(goal)
   }
+
+  const completeGoal = index => {
+    let finishedGoal = goals.filter(item => item.key == index)
+    setFinishedGoal(finishedGoals => [...finishedGoal, ...finishedGoals])
+    let goal = goals.filter(item => item.key != index)
+    setGoal(goal)
+  }
+
+  const showHideFinished = () => {
+    setShowFinished(showFinished => !showFinished)
+  } 
+
+  const finishedGoalsContainer = finishedGoals.length ?
+    <FlatList data={finishedGoals} renderItem={itemData => (
+      <View style={{ flexDirection: "row", alignContent: "space-around", justifyContent: "center" }}>
+        <Text style={styles.goalFinishedText}>
+          {itemData.item.value}
+        </Text>
+      </View>)
+    }
+    extraData={finishedGoals}
+    />
+    : null
 
   // let allGoals = goals.map((goal, index) => <Text style={styles.goalText} key={index} onPress={index =>removeGoal(index)}>{index + 1}. {goal}</Text>).reverse()
   // const checkIcon = <Icon name="checksquareo" size={30} color="#900" style={styles.checkMark}/>;
@@ -27,7 +56,8 @@ export default function App() {
         <TextInput style={styles.textInput} placeholder="Course Goal" value={text} onChangeText={(text) => setText(text)} />
         <Button title="Click me!" onPress={addGoal} color="green" />
       </View>
-      <FlatList data={goals} renderItem={itemData => (
+     { goals.length ? <View style={styles.containerGoals}>
+        <FlatList data={goals} renderItem={itemData => (
         <View style={{ flexDirection: "row", alignContent: "space-around", justifyContent: "center" }}>
           <Text style={styles.goalText}>{itemData.item.value} </Text>
           {/* {checkIcon}  */}
@@ -37,7 +67,7 @@ export default function App() {
             color="green"
             style={styles.checkMark}
             backgroundColor="none"
-            onPress={removeGoal.bind(this, itemData.item.key)}>
+            onPress={completeGoal.bind(this, itemData.item.key)}>
           </Icon.Button>
           <Icon.Button
             name="minussquare"
@@ -49,6 +79,23 @@ export default function App() {
           </Icon.Button>
         </View>)}
         extraData={goals} />
+      </View> : null }
+      {finishedGoals.length ? <View style={styles.containerGoals}>
+        <View style={{ flexDirection: "row", alignContent: "center", justifyContent: "center"}}>
+          <Text style={styles.headerFinishedContainer} onPress={showHideFinished}>Finished</Text>
+          <Icon.Button
+            name="minussquare"
+            size={30}
+            color="red"
+            style={styles.deleteMark}
+            backgroundColor="none"
+            onPress={()=> setFinishedGoal([])}>
+          </Icon.Button>
+        </View>
+        {showFinished ? 
+        { ...finishedGoalsContainer }        
+        : null }
+        </View> : null}
     </View>
   );
 }
@@ -56,6 +103,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     padding: 30
+  },
+  containerGoals: {
+    marginVertical:10,
+    borderWidth:1,
+    borderRadius:5,
+    padding: 10
   },
   header: {
     flexDirection: "row",
@@ -78,10 +131,28 @@ const styles = StyleSheet.create({
     padding: 10,
     flex: 4
   },
+  goalFinishedText: {
+    backgroundColor: '#DA70D6',
+    borderRadius: 5,
+    marginVertical: 5,
+    padding: 10,
+    flex: 4,
+    textDecorationLine: "line-through"
+  },
   checkMark: {
     flex: 1
   },
   deleteMark: {
     flex: 1
+  },
+  headerFinishedContainer: {
+    flex:4,
+    backgroundColor:'#9ACD32',
+    borderRadius:5,
+    paddingLeft:10,
+    paddingTop:15,
+    textAlignVertical:"center",
+    fontSize:16,
+    fontWeight:"500"
   }
 });
